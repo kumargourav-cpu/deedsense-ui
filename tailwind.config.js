f795da0ab44ef64761bf09155e173432ab16a7cd
kumargourav-cpu <kumar.eqglobal@gmail.com>
2026-02-19 15:08:50 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
-  plugins: [],
+  theme: {
+    extend: {
+      borderRadius: {
+        xl: "14px",
+        "2xl": "18px"
+      }
+    }
+  },
+  plugins: []
 };
